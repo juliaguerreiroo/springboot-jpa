66a6ee11c5b6ec09f9296dd29583a8c10666358d
juliaguerreiroo <judalanorag@gmail.com>
2026-04-25 00:51:00 -0300

    project created
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/julia/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
